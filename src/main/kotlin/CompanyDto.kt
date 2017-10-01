e126f1b08d2538090d2b909afb8a4fd8ee79830a
olexandr <kutovio@gmail.com>
2017-10-02 00:05:02 +0300

    Read from json to list of objects

---
+++ src/main/kotlin/CompanyDto.kt
@@ -0,0 +1,6 @@
+data class CompanyDto(
+        val company_name: String,
+        val job_title: String,
+        val longitude: Double,
+        val latitude: Double
+)
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,19 @@
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.io.File
+
 class Main {
     companion object {
+        private var gson = Gson()
+
         @JvmStatic
         fun main(args: Array<String>) {
             println("HELLO")
+            val companiesList = gson.fromJson<ArrayList<CompanyDto>>(File("dataset.json").readText(),
+                    object : TypeToken<ArrayList<CompanyDto>>() {}.type)
+                    as ArrayList<CompanyDto>
+
+            companiesList.forEach { println("COMPANY : $it") }
         }
     }
 }
